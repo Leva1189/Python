cca78e6b64e9aff6884f2dfdb65f215f0f6efff9
Dmytro Levchenko <levchenkodm13@gmail.com>
2025-10-09 15:55:49 +0300

    Lesson-4

---
+++ unit1/4.Lesson.py
@@ -0,0 +1,34 @@
+import random
+
+# random_number = random.randint(1, 100)
+# user_answer = int(input("Введіть число від 1 до 100: "))
+
+# while True:
+#     if user_answer < random_number:
+#         print("Не вгадав, твоє число менше")
+#         user_answer = int(input("Спробуй ще раз: "))
+#     elif user_answer > random_number:
+#         print("Не вгадав, твоє число більше")
+#         user_answer = int(input("Спробуй ще раз: "))
+#     else:
+#         print("Вітаю! Ти вгадав число.")
+#         break
+
+
+#==============================================
+# random while
+#==============================================
+# random_number = random.randint(1, 100)
+
+# while True:
+#   print(f"Вітаю переможця = {random_number}")
+#   break
+
+#==============================================
+# random for
+#==============================================
+# random_number = random.randint(1, 100)
+
+# for i in range(random_number):
+#     print(f"Вітаю з перемогою {random_number}:")
+#     break
