54812d056491a6335c11efe720063c7a420d2ba8
Dmytro Levchenko <levchenkodm13@gmail.com>
2025-10-17 17:09:49 +0300

    Lesson-6

---
+++ unit1/6.Lesson.py
@@ -0,0 +1,17 @@
+# list_names = ["Дмитро", "Ден", "Ігор", "Олег"]
+# list_names[1] = 'Petrov'
+# list_names.append('Petrov')
+# list_names.insert(1, 'Ivan')
+# list_names.remove('Petrov')
+# list_names.remove('Petrov')
+
+# list_names_copy = list_names.copy()
+
+
+# del list_names[1] # видаляє елемент за індексом
+
+# got_name = list_names.pop(0) # вирізає елемент за індексом і повертає його
+
+# print(list_names)
+# print(list_names_copy)
+# print(got_name)
